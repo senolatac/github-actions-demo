becf4f6bdd192780461bcf523ad97e3a6fd5810b
Senol Atac <senol.atac.1@gmail.com>
2022-07-06 17:11:16 +0300

    initial

---
+++ src/main/java/com/example/demogithubaction/DemoGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubActionApplication.class, args);
+	}
+
+}
